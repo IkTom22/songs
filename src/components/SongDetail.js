a5f090d5f007e5247deeacad5aad17b5a2be393d
Iko <ikotominaga@gmail.com>
2019-03-27 13:25:16 +1300

    selected song detail displays

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
-    console.log(props);
-    return <div>Song Detail</div>
+// instead of passing 'props', destructure {song}
+const SongDetail = ({ song }) => {
+
+    if (!song) {
+        return <div>Select a song</div>
+    }
+    // console.log(props);
+    return (
+
+        <div>
+            <h3>Details for:</h3>
+            <p>
+                Title: {song.title}
+                <br />
+                Duration: {song.duration}
+            </p>
+
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => {
     //don't have to call the key name 'selectedSong'
-    return { mySelectedSong: state.selectedSong }
+    // This is the props info goes to SongDetail
+    return { song: state.selectedSong }
 };
 
 export default connect(mapStateToProps)(SongDetail);
